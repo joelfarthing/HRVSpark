b7955bb3fa61bb0d7027b71d4e34fddcda34b290
joel <farthing@me.com>
2026-02-22 10:20:29 -0600

    Initial Commit

---
+++ HRVSpark/HRVSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRVSparkApp.swift
+//  HRVSpark
+//
+//  Created by Joel Farthing on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct HRVSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
